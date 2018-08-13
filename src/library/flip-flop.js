a8d512eb6ffd7b38f12069089c546b8fd4c92606
Zachary Berry <zachberry@gmail.com>
2018-08-12 22:28:48 -0400

    * Added examples

--- src/library/flip-flop.js
+++ src/library/flip-flop.js
@@ -31,40 +31,6 @@ class FlipFlop extends N {
 		return ["out"];
 	}
 
-	updateAndSendValue() {
-		let value = this.getAttribute("value");
-		let isAscending = this.getAttribute("ascending");
-		let byAmount = this.getAttribute("by-amount");
-		let loopAtMax = this.getAttribute("loop-at-max");
-		let max = this.getAttribute("max");
-		let operator = this.getAttribute("operator");
-
-		if (!isAscending) byAmount *= -1;
-
-		switch (operator) {
-			case "add":
-				value += byAmount;
-				break;
-
-			case "multiply":
-				value *= byAmount;
-				break;
-
-			case "divide":
-				value /= byAmount;
-				break;
-		}
-
-		if (loopAtMax) {
-			if ((isAscending && value > max) || (!isAscending && value < max)) {
-				value = this.getAttribute("start");
-			}
-		}
-
-		this.setAttribute("value", value);
-		this.send("out", value);
-	}
-
 	onAttrChanged(name, oldValue, newValue) {
 		let v1 = this.getAttribute("value-1");
 		let v2 = this.getAttribute("value-2");
